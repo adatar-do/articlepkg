d04f9687143a04753eeaf4460e2f1edb1cbf0a3f
Daniel de la Rosa <drdsdaniel@gmail.com>
2021-11-02 13:01:17 -0400

    feat (create_article_pkg): use_home argument

--- R/create_article_pkg.R
+++ R/create_article_pkg.R
@@ -6,6 +6,7 @@
 #' @param template base document to use. You can go with the default. Or set any
 #'   of the \href{https://pkgs.rstudio.com/rticles/}{rticles} formats.
 #'   Theoretically any format that follows the conventions of rticles can be used.
+#' @param use_home indicates if home page is used to display the article online version
 #' @param ... other arguments passed to \code{usethis::\link[usethis:create_package]{create_package}}
 #'
 #' @return [NULL]
@@ -13,25 +14,41 @@
 #'
 #' @examples
 #' \dontrun{
-#'   create_article_pkg("~/foofactors")
+#' create_article_pkg("~/foofactors")
 #' }
 create_article_pkg <- function(path,
-                             template = Rmdx::rmdx_paper,
-                             ...
-                            ){
+                               template = Rmdx::rmdx_paper,
+                               use_home = TRUE,
+                               ...) {
   pkg <- as.character(substitute(template))[[2]]
 
   fn <- as.character(substitute(template))[[3]]
 
   path <- suppressWarnings(normalizePath(path))
 
+  if(stringr::str_detect(path, "/")){
+    pp <- stringr::str_split(path, "/")[[1]]
+  } else {
+    pp <- stringr::str_split(path, "\\\\")[[1]]
+  }
+
+  name <- pp[length(pp)]
+
   usethis::create_package(path = path, open = FALSE, ...)
 
   setwd(path)
 
-  dir.create(paste0(path, "/article"))
+  if (use_home) {
+    dest <- "article"
+    dir.create(paste0(path, paste0("/", dest)))
+  } else {
+    dest <- "vignettes"
+    dir.create(paste0(path, paste0("/", dest)))
+  }
 
-  pkg_resource <-  function(pkg0 = pkg, ...) {system.file(..., package = pkg0)}
+  pkg_resource <- function(pkg0 = pkg, ...) {
+    system.file(..., package = pkg0)
+  }
 
   templates <- list.dirs(
     paste0(pkg_resource(), "/rmarkdown/templates"),
@@ -49,24 +66,46 @@ create_article_pkg <- function(path,
   for (fil in files_list) {
     file.copy(
       paste0(pkg_resource(), "/rmarkdown/templates/", temp_dir, "/skeleton/", fil),
-      paste0(path, "/", "article/", fil)
+      paste0(path, "/", dest, "/", fil)
     )
   }
 
-  rmd <-  paste0(pkg_resource(), "/rmarkdown/templates/", temp_dir, "/skeleton/skeleton.Rmd")
+  rmd <- paste0(pkg_resource(), "/rmarkdown/templates/", temp_dir, "/skeleton/skeleton.Rmd")
   rmd <- readr::read_file(rmd)
   rmd <- stringr::str_replace_all(rmd, "\r\n", "\n")
   inlines <- stringr::str_split(rmd, "\n")[[1]]
   output_opts <- trimws(
     inlines[match(1, stringr::str_detect(inlines, "output:"))]
-    ) == "output:"
-  rmd <- stringr::str_replace(rmd,
-                              "\n---",
-                              paste0(ifelse(output_opts, "\n", ": default\n"),
-        "knit: articlepkg::knit",
-        "\n---"))
-  rmd <- stringr::str_replace(rmd,
-                              "output: ",
-                              "output:\n  rmarkdown::github_document: default\n  ")
-  readr::write_file(rmd, file = paste0(path, "/article/index.Rmd"))
+  ) == "output:"
+  rmd <- stringr::str_replace(
+    rmd,
+    "\n---",
+    paste0(
+      ifelse(output_opts, "\n", ": default\n"),
+      "knit: articlepkg::knit",
+      "\n---"
+    )
+  )
+  rmd <- stringr::str_replace(
+    rmd,
+    "output: ",
+    "output:\n  rmarkdown::github_document: default\n  "
+  )
+  if (!use_home) {
+    rmd <- stringr::str_replace(
+      rmd,
+      "rmarkdown::github_document",
+      "rmarkdown::html_vignette"
+    )
+    rmd <- stringr::str_replace(
+      rmd,
+      "knit: articlepkg::knit",
+      paste0(
+        "vignette: >\n  \\%\\\\VignetteIndexEntry{", name, "}\n  ",
+        "\\%\\\\VignetteEngine{knitr::rmarkdown}\n  ",
+        "\\%\\\\VignetteEncoding{UTF-8}\nknit: articlepkg::knit"
+      )
+    )
+  }
+  readr::write_file(rmd, file = paste0(path, "/", dest,"/", ifelse(use_home, "index", name), ".Rmd"))
 }
